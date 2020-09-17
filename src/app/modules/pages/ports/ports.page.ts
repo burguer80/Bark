0ac38d6a57fde9b8c3da96a856f7d141a4c1a3a2
Sergio Burgueno <sergio.burgueno@unosquare.com>
2020-09-16 21:20:04 -0700

    ⚡ download on toggle

--- src/app/modules/pages/ports/ports.page.ts
+++ src/app/modules/pages/ports/ports.page.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 
 
+import {PwtEntityService} from '../../store/pwt/pwt-entity.service';
 import {Port} from '../../shared/models/port.model';
 import {PortEntityService} from '../../store/port/port-entity.service';
+import {first} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-ports',
-  templateUrl: './ports.page.html',
-  styleUrls: ['./ports.page.scss'],
+    selector: 'app-ports',
+    templateUrl: './ports.page.html',
+    styleUrls: ['./ports.page.scss'],
 })
 export class PortsPage implements OnInit {
-  public ports$: Observable<Port[]> = this.portFacade.entities$;
+    public ports$: Observable<Port[]> = this.portFacade.entities$;
 
-  constructor(
-      private portFacade: PortEntityService,
-  ) { }
+    constructor(
+        private portFacade: PortEntityService,
+        private pwtFacade: PwtEntityService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    getPwt(portId): void {
+        console.log(portId);
+        this.pwtFacade.getFirstOrLoadPwt(portId).pipe(
+            first()
+        ).subscribe();
+    }
 
 }
