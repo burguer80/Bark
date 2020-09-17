0f35c6d036ef1851cce734dd2afc1929c4779417
Sergio Burgueno <sergio.burgueno@unosquare.com>
2020-09-16 18:30:49 -0700

    ⚡ load pwt if needed via route param

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {PortEntityService} from '../modules/store/port/port-entity.service';
+import {PwtEntityService} from '../modules/store/pwt/pwt-entity.service';
+import { map} from 'rxjs/operators';
+import {Pwt} from '../modules/shared/models/pwt.model';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +13,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  public pwts: Observable<Pwt[]> = this.pwtFacade.entities$;
+  public pwt$: Observable<Pwt>;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+      private activatedRoute: ActivatedRoute,
+      private portFacade: PortEntityService,
+      private pwtFacade: PwtEntityService
+      ) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(this.folder);
+    // this.pwtFacade.loading$.pipe(
+    //     tap(loading => console.log(loading))
+    // ).subscribe();
+
+    // this.pwt$ = this.pwtFacade.entities$.pipe(
+    //     map(portWaitTimes => portWaitTimes.find((pwt: Pwt) => pwt.port_number === '250301'))
+    // );
+
+    this.pwt$ = this.pwtFacade.filteredEntities$.pipe(
+        map(portWaitTimes => portWaitTimes.find((pwt: Pwt) => pwt.port_number === '250301'))
+    );
   }
 
 }
