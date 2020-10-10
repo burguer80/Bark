0f090376747f449959f99580b6c11579d3d15202
Sergio Burgueño <burguer@gmail.com>
2020-10-09 21:29:45 -0700

    wip

--- src/app/modules/pages/pwt-list/pwt-list.module.ts
+++ src/app/modules/pages/pwt-list/pwt-list.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { PwtListPageRoutingModule } from './pwt-list-routing.module';
+import {PwtListPageRoutingModule} from './pwt-list-routing.module';
 
-import { PwtListPage } from './pwt-list.page';
+import {PwtListPage} from './pwt-list.page';
+import {PwtComponent} from './pwt/pwt.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    PwtListPageRoutingModule
-  ],
-  declarations: [PwtListPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        PwtListPageRoutingModule
+    ],
+    declarations: [PwtListPage, PwtComponent]
 })
-export class PwtListPageModule {}
+export class PwtListPageModule {
+}
---
+++ src/app/modules/pages/pwt-list/pwt/pwt.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pwt',
+  templateUrl: './pwt.component.html',
+  styleUrls: ['./pwt.component.scss'],
+})
+export class PwtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
