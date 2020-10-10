7563b1a88dd53e434204a14837245a9ad95534ea
Sergio Burgueno <sergio.burgueno@unosquare.com>
2020-10-10 12:53:22 -0700

    ⚡ removed unused pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'ports',
+    redirectTo: 'pwt-list',
     pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule),
-
   },
   {
     path: 'ports',
