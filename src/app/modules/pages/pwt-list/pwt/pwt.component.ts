693b9180816644a47e04bd512d76201a80cedcbb
Sergio Burgueño <burguer@gmail.com>
2020-10-28 22:14:55 -0700

    ⚡️ improve the minuteTohours method

--- src/app/modules/pages/pwt-list/pwt/pwt.component.ts
+++ src/app/modules/pages/pwt-list/pwt/pwt.component.ts
@@ -54,12 +54,27 @@ export class PwtComponent implements OnInit {
     }
 
     public minutesToHours(minutes: string): string {
-        return !!minutes ? moment.duration(minutes, 'minutes').format('h[h] m[m]') : '-';
+        if (!minutes) {
+            return '-';
+        }
+
+        const minsNumber = Number(minutes);
+
+        if (minsNumber < 60) {
+            return `${minutes}m`;
+        }
+
+        if (minsNumber === 60) {
+            return moment.duration(minutes, 'minutes').format('h[h]');
+        }
+
+        if (minsNumber > 60) {
+            return moment.duration(minutes, 'minutes').format('h[h] m[m]');
+        }
     }
 
 }
 
-// TODO: convert minutes into readable format
 // TODO: apply ngclass to call method in order to show display the proper color red/green
-// TODO: add moment js to display ago time.
-// TODO: define what is the best option to get the latest wait time 1. crazy map vs 2. endpoint
+// TODO: extract momentjs logic to a service
+// TODO: this code must be refactored is super ugly
