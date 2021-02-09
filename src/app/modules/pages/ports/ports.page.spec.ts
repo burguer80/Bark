30a09b01ef56250a5d2564f0a4c0080ede0e260f
Sergio Burgueño <burguer@gmail.com>
2021-02-08 17:56:27 -0800

    updated dependencies

--- src/app/modules/pages/ports/ports.page.spec.ts
+++ src/app/modules/pages/ports/ports.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { PortsPage } from './ports.page';
@@ -7,7 +7,7 @@ describe('PortsPage', () => {
   let component: PortsPage;
   let fixture: ComponentFixture<PortsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortsPage ],
       imports: [IonicModule.forRoot()]
