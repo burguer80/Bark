8dbcc4f16f8796f45bbdc68d75bfad8303aa8da6
Sergio Burgueño <burguer@gmail.com>
2021-02-09 20:15:27 -0800

    removed NGRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {EntityStoreModule} from './modules/store/entity-store.module';
 import {LoadingComponent} from './src/app/modules/shared/loading/loading.component';
 
 @NgModule({
@@ -20,7 +19,6 @@ import {LoadingComponent} from './src/app/modules/shared/loading/loading.compone
         IonicModule.forRoot(),
         AppRoutingModule,
         HttpClientModule,
-        EntityStoreModule
     ],
     providers: [
         StatusBar,
